31f78406055c4c2ea09ce50bffca55f55a0a50db
yokoto <youkotoushi@gmail.com>
2021-07-17 16:01:35 +0900

    first commit and add Timeline.vue

---
+++ src/components/Timeline.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a>Today</a>
+      <a>This Week</a>
+      <a>This Month</a>
+    </span>
+  </nav>
+  <h1>Timeline</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Timeline',
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
