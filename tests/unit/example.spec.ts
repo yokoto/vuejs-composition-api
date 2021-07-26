108a3e9622f65aa29f05231dfd3a77391cc69017
yokoto <youkotoushi@gmail.com>
2021-07-27 00:31:22 +0900

    add second Testing with Vue Test Utils

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,29 @@
 import { mount } from '@vue/test-utils'
 import Timeline from '../../src/components/Timeline.vue'
-import { today } from '../../src/mocks'
+import { today, thisWeek, thisMonth } from '../../src/mocks'
 
 describe('Timeline.vue', () => {
   it('renders today post default', () => {
     const wrapper = mount(Timeline)
 
-    console.log(wrapper.html())
     expect(wrapper.html()).toContain(today.created.format('Do MMM'))
+  })
+
+  it('update when the period is click', async () => {
+    const wrapper = mount(Timeline)
+
+    await wrapper.get('[data-test="This Week"]').trigger('click')
+
+    expect(wrapper.html()).toContain(today.created.format('Do MMM'))
+    expect(wrapper.html()).toContain(thisWeek.created.format('Do MMM'))
+  })
+
+  it('update when the period is click', async () => {
+    const wrapper = mount(Timeline)
+
+    await wrapper.get('[data-test="This Month"]').trigger('click')
+
+    expect(wrapper.html()).toContain(today.created.format('Do MMM'))
+    expect(wrapper.html()).toContain(thisMonth.created.format('Do MMM'))
   })
 })
