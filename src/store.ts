f7c900c8afb965a56b422cfc0f60189dfcea7f77
yokoto <youkotoushi@gmail.com>
2021-07-29 00:52:53 +0900

    add datastructure to state

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,18 @@
 import { reactive, readonly } from 'vue'
+import { Post } from './mocks'
 
 interface State {
-  test: string
+  posts: PostsState
+}
+
+interface PostsState {
+  // o(n)
+  ids: string[]
+
+  // o(1)
+  all: Map<string, Post>
+
+  loaded: boolean
 }
 
 class Store {
@@ -14,12 +25,14 @@ class Store {
   getState() {
     return readonly(this.state)
   }
-
-  updateTest(test: string) {
-    this.state.test = test
-  }
 }
 
 const store = new Store({
-  test: 'test'
+  posts: {
+    all: new Map(),
+    ids: [],
+    loaded: false
+  }
 })
+
+store.getState().posts.loaded
