683e66ab245116362b2e430248120bcc0091e759
yokoto <youkotoushi@gmail.com>
2021-07-31 03:48:15 +0900

    add custom events and saving post

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -15,10 +15,17 @@
   <div class="columns">
     <div class="column">
       <div contenteditable ref="contentEditable" @input="handleInput" />
-
     </div>
     <div class="column">
       <div v-html="html" />
+    </div>
+  </div>
+
+  <div class="columns">
+    <div class="column">
+      <button @click="save" class="button is-primary is-pulled-right">
+        Submit
+      </button>
     </div>
   </div>
 </template>
@@ -38,7 +45,13 @@ export default defineComponent({
     }
   },
 
-  setup(props) {
+  emits: {
+    save: (post: Post) => {
+      return true
+    }
+  },
+
+  setup(props, ctx) {
     const title = ref(props.post.title)
     const content = ref('## Title\nEnter your post content...')
     const contentEditable = ref<HTMLDivElement | null>(null)
@@ -73,7 +86,20 @@ export default defineComponent({
 
       contentEditable.value.innerText = content.value
     })
+
+    const save = () => {
+      const newPost: Post = {
+        ...props.post,
+        title: title.value,
+        html: html.value,
+        markdown: content.value,
+      }
+
+      ctx.emit('save', newPost)
+    }
+
     return {
+      save,
       html,
       title,
       content,
--- src/mocks.ts
+++ src/mocks.ts
@@ -4,6 +4,8 @@ export interface Post {
   id: string
   title: string
   created: moment.Moment
+  html?: string
+  markdown?: string
 }
 
 export const today: Post = {
