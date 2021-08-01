8c0850f9421cbda06a4933ad87296ecbaa3e9c40
yokoto <youkotoushi@gmail.com>
2021-08-02 01:07:21 +0900

    add v-model with Custom Components

---
+++ src/components/FormInput.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="field">
+    <label class="label" :name="name">{{ name }}</label>
+    <div class="control">
+      <input
+        class="input"
+        :type="type"
+        v-model="modelValue"
+        @input="handleInput"
+      />
+      <p class="is-danger help">{{ error }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    modelValue: {
+      type: String,
+      required: true
+    },
+    error: {
+      type: String,
+    }
+  },
+  setup(props, ctx) {
+    const handleInput = (e: Event) => {
+      ctx.emit('update:modelValue', (e.target as HTMLInputElement).value)
+    }
+
+    return {
+      handleInput
+    }
+  },
+})
+</script>
