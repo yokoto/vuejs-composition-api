d38fd1bbad7526b7479db26ba1804e50b4a0eb4a
yokoto <youkotoushi@gmail.com>
2021-08-10 21:06:08 +0900

    add dynamic components

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,32 +1,56 @@
 <template>
   <div class="navbar">
     <div class="navbar-end">
-      <div class="buttons">
-        <button
-          class="button"
-          @click="show"
-        >
-          Sign Up
-        </button>
+      <div
+        class="buttons"
+        v-if="auth"
+      >
         <router-link
           class="button"
           to="/posts/new"
         >
           New Post
         </router-link>
+
+        <button
+          class="button"
+          @click="signOut"
+        >
+          Sign Out
+        </button>
+      </div>
+
+      <div
+        class="buttons"
+        v-else
+      >
+        <button
+          class="button"
+          @click="signUp"
+        >
+          Sign Up
+        </button>
+
+        <button
+          class="button"
+          @click="signIn"
+        >
+          Sign In
+        </button>
       </div>
     </div>
   </div>
 
   <teleport to="#modal">
-    <signup />
+    <component :is="component" />
   </teleport>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, h } from 'vue';
 import Signup from '../components/Signup.vue'
 import { useModal } from '../useModal'
+import { useStore } from '../store'
 
 export default defineComponent({
   components: {
@@ -34,8 +58,33 @@ export default defineComponent({
   },
   setup() {
     const modal = useModal()
+    const store = useStore()
+
+    const auth = computed(() => {
+      return !!store.getState().authors.currentUserId
+    })
+
+    const signIn = () => {
+      const Demo = defineComponent({
+        setup() {
+          return () => h('div', 'Demo')
+        }
+      })
+      modal.component.value = Demo
+      modal.showModal()
+    }
+    const signUp = () => {
+      modal.component.value = Signup
+      modal.showModal()
+    }
+    const signOut = () => {}
 
     return {
+      component: modal.component,
+      signIn,
+      signUp,
+      signOut,
+      auth,
       show: () => {
         modal.showModal()
       }
