c1c01315b6c4bba39be17dc0c066ae8cb6cc2f89
yokoto <youkotoushi@gmail.com>
2021-08-10 23:14:13 +0900

    add access params with useRoute

---
+++ src/components/ShowPost.vue
@@ -0,0 +1,22 @@
+<template>
+  Show Post
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useRoute } from 'vue-router'
+import { useStore } from '../store'
+
+export default defineComponent({
+  setup(props) {
+    const store = useStore()
+    const id = useRoute().params.id as string
+    const post = store.getState().posts.all.get(id)
+    console.log(post)
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
